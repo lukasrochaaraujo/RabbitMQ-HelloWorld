353ad1f96f59db26d106d6084238d4094048d558
Jorge Lucas <lukasrochaaraujo@hotmail.com>
2021-06-19 08:40:56 -0300

    Created new Worker and NewTask projects

---
+++ NewTask/NewTask.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace NewTask
+{
+    class NewTask
+    {
+        static void Main(string[] args)
+        {
+            ConnectionFactory connectionFactory = new ConnectionFactory{ HostName = "localhost" };
+			using IConnection connection = connectionFactory.CreateConnection();
+			using IModel channel = connection.CreateModel();
+			
+			channel.QueueDeclare(queue: "task_queue", 
+								 durable: false, 
+								 exclusive: false, 
+								 autoDelete: false, 
+								 arguments: null);
+
+            string message = GetMessage(args);
+            byte[] body = Encoding.UTF8.GetBytes(message);
+
+            var properties = channel.CreateBasicProperties();
+            properties.Persistent = true;
+
+            channel.BasicPublish(exchange: "",
+                                 routingKey: "task_queue",
+                                 basicProperties: properties,
+                                 body: body);
+        }
+
+        private static string GetMessage(string[] args)
+            => ((args.Length > 0) ? string.Join(" ", args) : "Hello World!");
+    }
+}
--- Worker/Worker.cs
+++ Worker/Worker.cs
@@ -1,11 +1,12 @@
-﻿using RabbitMQ.Client;
-using RabbitMQ.Client.Events;
-using System;
+﻿using System;
 using System.Text;
+using System.Threading;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
 
-namespace Receive
+namespace Worker
 {
-    class Receive
+    class Worker
     {
         static void Main(string[] args)
         {
@@ -13,23 +14,32 @@ namespace Receive
             using var connection = connectionFactory.CreateConnection();
             using var channel = connection.CreateModel();
 
-            channel.QueueDeclare(queue: "hello",
+            channel.QueueDeclare(queue: "task_queue",
                                  durable: false,
                                  exclusive: false,
                                  autoDelete: false,
                                  arguments: null);
             
+            channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
+
+            Console.WriteLine(" [*] Waiting for messages.");
+
             var consumer = new EventingBasicConsumer(channel);
             consumer.Received += (ModuleHandle, eventArgs) =>
             {
                 var body = eventArgs.Body.ToArray();
                 var message = Encoding.UTF8.GetString(body);
                 Console.WriteLine($" [x] Received {message}");
+
+                int dots = message.Split('.').Length - 1;
+                Thread.Sleep(dots * 1000);
+
+                channel.BasicAck(deliveryTag: eventArgs.DeliveryTag, multiple: false);
+
+                Console.WriteLine(" [x] Done");
             };
 
-            channel.BasicConsume(queue: "hello", 
-                                  autoAck: true, 
-                                  consumer: consumer);
+            channel.BasicConsume(queue: "task_queue", autoAck: false, consumer: consumer);
 
             Console.WriteLine(" Press [enter] to exit.");
             Console.ReadLine();
